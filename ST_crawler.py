e804baee05bdaca7c78999afb8c16b4643a27282
Henry Wang <w.henry4@live.com>
2020-03-25 13:03:46 -0700

    modifed crawler to use mongo

--- ST_crawler.py
+++ ST_crawler.py
@@ -1,26 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 24 18:02:56 2020
-
-@author: Tang
-"""
-
 import requests
 import json
 import os
-
+from pymongo import MongoClient
+from time import sleep
 
 class ST_crawler:
-    def __init__ (self):
+    def __init__ (self, db_url="mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"):
         self.url = "https://api.stocktwits.com/api/2/streams/symbol/{}.json"
-        self.tweets = []
-        self.labels = []
+        self.client = MongoClient(db_url)
+        self.db = self.client.StockTwits
         self.targetDates = "consider a range of time for the future"
 
-        if not os.path.isdir("stocks"):
-            os.mkdir("stocks")
-    
-    def get_tweets(self,ticker):
+    def get_tweets(self, ticker):
         req_url = self.url.format(ticker)
         try:
             response = requests.get(req_url)
@@ -32,26 +23,34 @@ class ST_crawler:
         except:
             print("json decode error!")
     
-
         if data and data["response"]["status"] == 200:
+            twits = []
             for s in data["messages"]:
+                if self.db.twits.find_one({"_id": s["id"]}):
+                    # twit already exists
+                    continue
                 # each individual message dict --- s is a dict=
                 text = s["body"]
                 text = text.replace("\n", " ").strip()
                 label = s["entities"]["sentiment"]["basic"] if s["entities"]["sentiment"] else ""
-                self.tweets.append(text)
-                self.labels.append(label)
+                twit = {
+                    "_id": s["id"],
+                    "body": text,
+                    "timestamp": s["created_at"],
+                    "label": label
+                }
+                
+                twits.append(twit)
+            if twits:
+                self.db.twits.insert_many(twits)
+                print(f"inserted {len(twits)} twits")
+        elif data["responce"]["status"] == 429:
+            raise Exception("Rate Limit Exceeded")
         else:
             print("bad json response")
 
-    def tweets_get_write(self, symbol):
-        self.get_tweets(symbol)
-        tw_fName = "stocks/" + symbol + "_tweet.txt"
-        lb_fName = "stocks/" + symbol + "_label.txt"
-        with open(tw_fName, "w",encoding='utf-8') as f:
-            f.write('\n'.join(self.tweets))
-        with open(lb_fName, "w",encoding='utf-8') as f:
-            f.write('\n'.join(self.labels))
-    
-app = ST_crawler()
-app.tweets_get_write("msft")
+if __name__ == "__main__":
+    app = ST_crawler()
+    while True: 
+        app.get_tweets("msft")
+        sleep(18)
