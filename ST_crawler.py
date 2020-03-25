d535bde2b397e2bdaa382471b548d8e52d1a017e
Henry Wang <w.henry4@live.com>
2020-03-25 15:35:08 -0700

    fixed datetime

--- ST_crawler.py
+++ ST_crawler.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import datetime
 from pymongo import MongoClient
 from time import sleep
 from database import db
@@ -9,6 +10,7 @@ class ST_crawler:
     def __init__ (self):
         self.url = "https://api.stocktwits.com/api/2/streams/symbol/{}.json"
         self.db = db
+        self.collection = self.db.twits
         self.targetDates = "consider a range of time for the future"
 
     def get_tweets(self, ticker):
@@ -40,25 +42,32 @@ class ST_crawler:
                 twit = {
                     "_id": s["id"],
                     "body": text,
-                    "timestamp": s["created_at"],
+                    "timestamp": self.convert_date(s["created_at"]),
                     "label": label,
                     "symbols": [sym["symbol"] for sym in s["symbols"]]
                 }
                 
                 twits.append(twit)
             if twits:
-                self.db.twits.insert_many(twits)
+                self.collection.insert_many(twits)
                 print(f"inserted {len(twits)} twits")
         elif data["responce"]["status"] == 429:
             raise Exception("Rate Limit Exceeded")
         else:
             print("bad json response")
 
+    def convert_date(self, date_string):
+        date_string = date_string[:-1]
+        date, time = date_string.split("T")
+        date = date.split("-")
+        time = time.split(":")
+        dt =[int(num) for num in date + time]
+        return datetime.datetime(*dt)
+
     def crawl(self, ticker, wait=18):
         while True:
             self.get_tweets(ticker)
             sleep(wait)
-
 
 if __name__ == "__main__":
     app = ST_crawler()
--- calculate.py
+++ calculate.py
@@ -4,3 +4,10 @@ Does the moving average calculation
 from pymongo import MongoClient
 from database import db
 
+class Calculator:
+    def __init__(self):
+        self.db = db
+        self.collection = self.db.averages
+    
+    def update(self):
+        pass
