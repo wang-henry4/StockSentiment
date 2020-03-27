8af89d483e9c32872430e5d24c5734818a6a1743
Henry Wang <w.henry4@live.com>
2020-03-26 19:54:51 -0700

    main.py and calculate.py

--- database.py
+++ database.py
@@ -8,3 +8,25 @@ with open('config.yaml') as f:
 
 client = MongoClient(config["db_url"])
 db=client.StockTwits
+
+class Ema_data:
+    def __init__(self, size = 288):
+        self.max_size = 288
+        self._data = [(doc["ema"], doc["end"]) 
+            for doc in db.averages.find().sort("end", -1).limit(288)][::-1]
+        self.size = len(self._data)
+
+    def update(self):
+        latest = db.averages.find_one({"latest": True})
+        self._data.append((latest["ema"], latest["end"]))
+        if len(self._data) > self.max_size:
+            dif = len(self._data) - self.max_size
+            self._data = self._data[dif:]
+        self.size = len(self._data)
+
+    @property
+    def data(self):
+        return list(zip(*self._data))
+
+    def __len__(self):
+        return self.size
